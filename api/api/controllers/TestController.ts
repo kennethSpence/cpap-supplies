e6b6bfdb3f49ac8a1b0d10be2f794faef3b28896
Kenneth Spence <kenneth.spence1@gmail.com>
2021-12-23 20:36:43 -0800

    added test sails route

---
+++ api/api/controllers/TestController.ts
@@ -0,0 +1,12 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  test: function (req:any, res:any) {
+    res.send('hello world');
+  },
+};
